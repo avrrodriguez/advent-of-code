eca7e9ff1778b055d737ca4955addb914fff33b9
Abraham V Rodriguez <pqavrr835@gmail.com>
2023-05-05 13:10:58 -0400

    Adds a whiteboarding and eliminates puts from day 6

--- day_6_tuning_trouble_p2.rb
+++ day_6_tuning_trouble_p2.rb
@@ -1,7 +1,6 @@
 def call(puzzle_input)
   uniq_chars = []
   puzzle_input.split("").each_with_index do |letter, index|
-    # p [letter, index, uniq_chars]
     uniq_chars = uniq_chars[uniq_chars.find_index(letter) + 1..-1] if uniq_chars.include?(letter)
     return index + 1 if uniq_chars.length == 13
 
@@ -10,5 +9,4 @@ def call(puzzle_input)
 end
 
 puzzle_input = File.readlines("./puzzle-inputs/day_6.txt").map(&:chomp)[0]
-# p puzzle_input
 p call(puzzle_input)
